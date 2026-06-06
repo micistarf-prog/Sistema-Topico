b9e9560d3af244f0d94035b06f365768615bb444
micitarf-prog <micistarf@gmail.com>
2026-06-05 21:12:54 -0500

    Actualizacion 1

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function irAdministrador() {
+
+    window.location.href =
+    "paginas/login-admin.html";
+
+}
+
+function irEncargado() {
+
+    window.location.href =
+    "paginas/login-encargado.html";
+
+}
